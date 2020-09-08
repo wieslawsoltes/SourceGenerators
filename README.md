890e18c5e7b76d645071c35372a47e75f1690042
wieslawsoltes <wieslaw.soltes@gmail.com>
2020-09-08 15:11:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Source Generators
+
+* https://github.com/dotnet/roslyn/blob/master/docs/features/source-generators.cookbook.md
+* https://github.com/dotnet/roslyn/blob/master/docs/features/source-generators.md
+* https://github.com/dotnet/roslyn-sdk/tree/master/samples/CSharp/SourceGenerators
+* https://github.com/dotnet/roslyn/blob/master/src/Test/Utilities/Portable/SourceGeneration/TestGenerators.cs
+* https://devblogs.microsoft.com/dotnet/introducing-c-source-generators/
---
+++ Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SkiaSharp;
+using Svg;
+using Svg.Skia;
+
+namespace Test
+{
+    class Program
+    {
+        static void Debug()
+        {
+            var svg = System.IO.File.ReadAllText("/home/ubuntu/projects/SourceGenerators/Test/Svg/e-ellipse-001.svg");
+            SvgDocument.SkipGdiPlusCapabilityCheck = true;
+            SvgDocument.PointsPerInch = 96;
+            var svgDocument = SvgDocument.FromSvg<SvgDocument>(svg);
+            if (svgDocument != null)
+            {
+                var picture = SKSvg.ToModel(svgDocument);
+                if (picture != null && picture.Commands != null)
+                {
+                    var text = SkiaCodeGenerator.Generate(picture, "e_ellipse_001");
+                    Console.WriteLine(text);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Debug();
+            //var ellipse = new e_ellipse_001();
+            //var rect = new e_rect_001();
+            //Console.WriteLine($"{ellipse.GetType()}");
+            //Console.WriteLine($"{ellipse.Message}");
+            //Console.WriteLine($"{rect.GetType()}");
+            //Console.WriteLine($"{rect.Message}");
+        }
+    }
+}
