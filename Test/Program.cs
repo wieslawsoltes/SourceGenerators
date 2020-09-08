98d76acd4344bac4a9a3415c0193bd1a5b168ea7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-09-08 20:39:28 +0200

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,22 +7,22 @@ namespace Test
 {
     class Program
     {
-        static void Debug()
-        {
-            var svg = System.IO.File.ReadAllText("/home/ubuntu/projects/SourceGenerators/Test/Svg/e-ellipse-001.svg");
-            SvgDocument.SkipGdiPlusCapabilityCheck = true;
-            SvgDocument.PointsPerInch = 96;
-            var svgDocument = SvgDocument.FromSvg<SvgDocument>(svg);
-            if (svgDocument != null)
-            {
-                var picture = SKSvg.ToModel(svgDocument);
-                if (picture != null && picture.Commands != null)
-                {
-                    var text = SkiaCodeGenerator.Generate(picture, "e_ellipse_001");
-                    Console.WriteLine(text);
-                }
-            }
-        }
+        //static void Debug()
+        //{
+        //    var svg = System.IO.File.ReadAllText("/home/ubuntu/projects/SourceGenerators/Test/Svg/e-ellipse-001.svg");
+        //    SvgDocument.SkipGdiPlusCapabilityCheck = true;
+        //    SvgDocument.PointsPerInch = 96;
+        //    var svgDocument = SvgDocument.FromSvg<SvgDocument>(svg);
+        //    if (svgDocument != null)
+        //    {
+        //        var picture = SKSvg.ToModel(svgDocument);
+        //        if (picture != null && picture.Commands != null)
+        //        {
+        //            var text = SkiaCodeGenerator.Generate(picture, "e_ellipse_001");
+        //            Console.WriteLine(text);
+        //        }
+        //    }
+        //}
 
         static void Main(string[] args)
         {
