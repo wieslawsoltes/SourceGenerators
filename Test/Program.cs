8f3666b10bbfc8f4e221fdf3b89e6fd5d21ec0c7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-09-08 19:29:06 +0200

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -26,7 +26,7 @@ namespace Test
 
         static void Main(string[] args)
         {
-            Debug();
+            //Debug();
             //var ellipse = new e_ellipse_001();
             //var rect = new e_rect_001();
             //Console.WriteLine($"{ellipse.GetType()}");
