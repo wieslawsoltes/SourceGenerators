b1e44e3e5fbeebaa093b7d4a40c937f2a3d1c695
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-09-08 22:20:35 +0200

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,26 +7,28 @@ namespace Test
 {
     class Program
     {
-        //static void Debug()
-        //{
-        //    var svg = System.IO.File.ReadAllText("/home/ubuntu/projects/SourceGenerators/Test/Svg/e-ellipse-001.svg");
-        //    SvgDocument.SkipGdiPlusCapabilityCheck = true;
-        //    SvgDocument.PointsPerInch = 96;
-        //    var svgDocument = SvgDocument.FromSvg<SvgDocument>(svg);
-        //    if (svgDocument != null)
-        //    {
-        //        var picture = SKSvg.ToModel(svgDocument);
-        //        if (picture != null && picture.Commands != null)
-        //        {
-        //            var text = SkiaCodeGenerator.Generate(picture, "e_ellipse_001");
-        //            Console.WriteLine(text);
-        //        }
-        //    }
-        //}
+        static void Debug()
+        {
+            var path = @"c:\DOWNLOADS\GitHub\SourceGenerators\Test\Svg\e-ellipse-001.svg";
+            //var path = "/home/ubuntu/projects/SourceGenerators/Test/Svg/e-ellipse-001.svg";
+            var svg = System.IO.File.ReadAllText(path);
+            SvgDocument.SkipGdiPlusCapabilityCheck = true;
+            SvgDocument.PointsPerInch = 96;
+            var svgDocument = SvgDocument.FromSvg<SvgDocument>(svg);
+            if (svgDocument != null)
+            {
+                var picture = SKSvg.ToModel(svgDocument);
+                if (picture != null && picture.Commands != null)
+                {
+                    var text = SkiaCodeGenerator.Generate(picture, "e_ellipse_001");
+                    Console.WriteLine(text);
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
-            //Debug();
+            Debug();
             var ellipse = new e_ellipse_001();
             var rect = new e_rect_001();
             Console.WriteLine($"{ellipse.GetType()}");
