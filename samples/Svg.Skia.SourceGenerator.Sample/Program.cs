f70aff22749c1b52c9e2777aa9b74b1c5767baa6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-09-10 19:35:05 +0200

    Update Program.cs

--- samples/Svg.Skia.SourceGenerator.Sample/Program.cs
+++ samples/Svg.Skia.SourceGenerator.Sample/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using SkiaSharp;
 using Svg;
+using Svg.Skia;
 
 namespace Svg.Skia.SourceGenerator.Sample
 {
@@ -7,10 +11,47 @@ namespace Svg.Skia.SourceGenerator.Sample
     {
         static void Main(string[] args)
         {
-            var ellipse = new e_ellipse_001();
-            var rect = new e_rect_001();
-            Console.WriteLine($"Generated class {ellipse.GetType()} from Svg file.");
-            Console.WriteLine($"Generated class {rect.GetType()} from Svg file.");
+            Console.WriteLine($"Generated class {typeof(Svg___AJ_Digital_Camera)} from Svg file.");
+            Console.WriteLine($"Generated class {typeof(Svg___tiger)} from Svg file.");
+            Console.WriteLine($"Generated class {typeof(Svg_e_ellipse_001)} from Svg file.");
+            Console.WriteLine($"Generated class {typeof(Svg_e_rect_001)} from Svg file.");
+            Console.WriteLine($"Generated class {typeof(Svg_pservers_pattern_01_b)} from Svg file.");
+
+            var sw = new Stopwatch();
+
+            sw.Start();
+            using var cameraStream = File.OpenWrite("__AJ_Digital_Camera.png");
+            Svg___AJ_Digital_Camera.Picture.ToImage(cameraStream, SKColors.Transparent, SKEncodedImageFormat.Png, 100, 1, 1, SKImageInfo.PlatformColorType, SKAlphaType.Unpremul);
+            sw.Stop();
+            Console.WriteLine($"Created __AJ_Digital_Camera.png in {sw.Elapsed.TotalMilliseconds}ms");
+
+            sw.Reset();
+            sw.Start();
+            using var tigerStream = File.OpenWrite("__tiger.png");
+            Svg___tiger.Picture.ToImage(tigerStream, SKColors.Transparent, SKEncodedImageFormat.Png, 100, 1, 1, SKImageInfo.PlatformColorType, SKAlphaType.Unpremul);
+            sw.Stop();
+            Console.WriteLine($"Created __tiger.png in {sw.Elapsed.TotalMilliseconds}ms");
+
+            sw.Reset();
+            sw.Start();
+            using var ellipseStream = File.OpenWrite("e-ellipse-001.png");
+            Svg_e_ellipse_001.Picture.ToImage(ellipseStream, SKColors.Transparent, SKEncodedImageFormat.Png, 100, 1, 1, SKImageInfo.PlatformColorType, SKAlphaType.Unpremul);
+            sw.Stop();
+            Console.WriteLine($"Created e-ellipse-001.png in {sw.Elapsed.TotalMilliseconds}ms");
+
+            sw.Reset();
+            sw.Start();
+            using var rectStream = File.OpenWrite("e-rect-001.png");
+            Svg_e_rect_001.Picture.ToImage(rectStream, SKColors.Transparent, SKEncodedImageFormat.Png, 100, 1, 1, SKImageInfo.PlatformColorType, SKAlphaType.Unpremul);
+            sw.Stop();
+            Console.WriteLine($"Created e-rect-001.png in {sw.Elapsed.TotalMilliseconds}ms");
+
+            sw.Reset();
+            sw.Start();
+            using var patternStream = File.OpenWrite("pservers-pattern-01-b.png");
+            Svg_pservers_pattern_01_b.Picture.ToImage(patternStream, SKColors.Transparent, SKEncodedImageFormat.Png, 100, 1, 1, SKImageInfo.PlatformColorType, SKAlphaType.Unpremul);
+            sw.Stop();
+            Console.WriteLine($"Created pservers-pattern-01-b.png in {sw.Elapsed.TotalMilliseconds}ms");
         }
     }
 }
