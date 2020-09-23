48573b8263f18a53b090d9c149bd07009e026853
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-09-24 00:15:10 +0200

    Use dotnet run

--- W3CTestSuite.sh
+++ W3CTestSuite.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
-
 mkdir temp
 cd ./temp
-
 git clone https://github.com/wieslawsoltes/SVG.git
-
-dotnet publish ../src/svgc/svgc.csproj -c Release -f netcoreapp3.1 -r win7-x64 /p:PublishTrimmed=True /p:PublishSingleFile=False /p:PublishReadyToRun=True -o ./svgc
-
 mkdir W3CTestSuite
-
-./svgc/svgc -j ../W3CTestSuite.json
-
+dotnet run -p ../src/svgc/svgc.csproj -c Release -- -j  ../W3CTestSuite.json
 dotnet new console -n W3CTestSuite -o W3CTestSuite
 dotnet add ./W3CTestSuite/W3CTestSuite.csproj package -v 2.80.2 SkiaSharp
 dotnet add ./W3CTestSuite/W3CTestSuite.csproj package -v 2.80.2 SkiaSharp.NativeAssets.Linux
--- resvg-test-suite.sh
+++ resvg-test-suite.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
-
 mkdir temp
 cd ./temp
-
 git clone https://github.com/wieslawsoltes/resvg-test-suite.git
-
-dotnet publish ../src/svgc/svgc.csproj -c Release -f netcoreapp3.1 -r win7-x64 /p:PublishTrimmed=True /p:PublishSingleFile=False /p:PublishReadyToRun=True -o ./svgc
-
 mkdir resvgtestsuite
-
-./svgc/svgc -j ../resvg-test-suite.json
-
+dotnet run -p ../src/svgc/svgc.csproj -c Release -- -j ../resvg-test-suite.json
 dotnet new console -n resvgtestsuite -o resvgtestsuite
 dotnet add ./resvgtestsuite/resvgtestsuite.csproj package -v 2.80.2 SkiaSharp
 dotnet add ./resvgtestsuite/resvgtestsuite.csproj package -v 2.80.2 SkiaSharp.NativeAssets.Linux
